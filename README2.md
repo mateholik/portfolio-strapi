b6448bdbebd66764b0c046c7977f9f334f5e1e04
Vladis macbook <vladuxs1@gmail.com>
2024-06-11 13:29:41 +0300

    populate js page data

--- README2.md
+++ README2.md
@@ -1,2 +1,7 @@
 reference video:
 https://www.youtube.com/watch?v=RceLeh9D85o&ab_channel=CodingAfterThirty
+
+- create a middleware: `yarn strapi generate`
+- name for middleware example: portfolio-page-populate
+- add populate object to middleware. query builder link: https://docs.strapi.io/dev-docs/api/rest/interactive-query-builder
+- update route to use middleware
--- src/api/javascript-page/routes/javascript-page.js
+++ src/api/javascript-page/routes/javascript-page.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * javascript-page router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::javascript-page.javascript-page');
+module.exports = createCoreRouter("api::javascript-page.javascript-page", {
+  config: {
+    find: {
+      middlewares: ["api::javascript-page.javascript-page-populate"],
+    },
+    findOne: {
+      middlewares: ["api::javascript-page.javascript-page-populate"],
+    },
+  },
+});
